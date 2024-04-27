0dae2bb350deb3c1f2b00363eb861098904546c5
essenlikcia <essenlikcia@gmail.com>
2024-04-27 14:51:58 +0300

    added functionality for scraping the corresponding author names

--- website/views.py
+++ website/views.py
@@ -4,7 +4,6 @@ import requests
 from django.shortcuts import render
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
-
 from .models import Record, Author
 
 
@@ -18,18 +17,15 @@ def home(request):
 
 
 load_dotenv()
+api_key = os.getenv('API_KEY')
 
 
 def search(request):
     if request.method == 'POST':
         query = request.POST.get('query')
         articles_number = int(request.POST.get('articles'))
-        api_key = os.getenv('API_KEY')
-
-        # Delete all records from the tables (assuming you want fresh data)
         Record.objects.all().delete()
         Author.objects.all().delete()
-
         url = f"https://api.elsevier.com/content/search/sciencedirect?query={query}&apiKey={api_key}"
 
         articles = []
@@ -55,33 +51,11 @@ def search(request):
                 date = item['prism:coverDate']
                 article_url = item['link'][1]['@href']
 
-                # Scrape corresponding author details using BeautifulSoup
-                response = requests.get(article_url)
-                soup = BeautifulSoup(response.content, 'html.parser')
+                pii = item['pii']
 
-                # Find the author information div
-                author_div = soup.find('div', class_='author-group')
+                corresponding_author_name = search_corresponding_author_name(pii)
+                corresponding_author_email = "N/A"
 
-                # Extract author names (assuming one author per button)
-                corresponding_author_names = []
-                if author_div:
-                    for button in author_div.find_all('button'):
-                        button_link_text_span = button.find('span', class_='button-link-text')  # Find the outer span
-                        if button_link_text_span:
-                            author_name_element = button_link_text_span.find('span', class_='react-xocs-alternative-link')  # Find the inner span
-                            if author_name_element:
-                                author_name = author_name_element.text.strip()
-                                corresponding_author_names.append(author_name)
-
-                corresponding_author_name = ', '.join(
-                    corresponding_author_names) if corresponding_author_names else 'N/A'
-
-                corresponding_author_email = None
-                email_element = soup.select_one("#side-panel-author > div.e-address > a > span")
-                if email_element:
-                    corresponding_author_email = email_element.text.strip()
-                else:
-                    corresponding_author_email = 'N/A'
                 papers.append({
                     'title': title,
                     'authors': authors,
@@ -110,3 +84,17 @@ def search(request):
     else:
         return render(request, 'home.html', {})
 
+
+def search_corresponding_author_name(pii):
+    url = f"https://api.elsevier.com/content/article/pii/{pii}?apiKey={api_key}"
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data, "lxml")
+    corresponding_author_tag = soup.find("ce:correspondence")
+    if not corresponding_author_tag:
+        return "N/A", "N/A"
+    author_tag = corresponding_author_tag.find_previous('ce:author')
+    given_name = author_tag.find('ce:given-name').text
+    surname = author_tag.find('ce:surname').text
+    corresponding_author_name = f"{given_name} {surname}"
+    return corresponding_author_name
